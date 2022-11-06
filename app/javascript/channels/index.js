7e3339214c4763107efd57a9c9785b4b025b446a
EC2 Default User <108612635+mihararyohei@users.noreply.github.com>
2022-11-06 09:32:16 +0000

    [update]

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,19 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+// Webページの動作を実装 ...（1）
+const MyFirstVue = {
+  data() { // データ ...（2）
+    return {
+      name: ''
+    }
+  },
+  methods: { // メソッド ...（3）
+    onButtonClicked() {
+    alert(`${this.name} さん、Vue.jsの世界にようこそ!`)
+    }
+  }
+}
+// Vue.jsを初期化 ...（4）
+Vue.createApp(MyFirstVue).mount('#my-first-vue')
